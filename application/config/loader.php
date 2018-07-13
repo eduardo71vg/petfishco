215f36030e38684487e08a6e4bbf1dacc9ff4ec4
Eduardo Hernandez <ppjehg@gmail.com>
2018-07-13 09:23:04 +0200

    Clean Up

--- application/config/loader.php
+++ application/config/loader.php
@@ -10,24 +10,13 @@ $loader->registerNamespaces(
 		"PetFishCo\Core" => APP_PATH . "/core",
 		'Phalcon\Http' => APP_PATH . '/Phalcon/Http',
 		'PetFishCo\Middlewares' => APP_PATH . '/middlewares',
-
-		//"PetFishCo\Core\Backend" => APP_PATH."/backend"
-
+		'PetFishCo\Core\Helpers' => APP_PATH . '/core/Helpers',
 	]
 );
 $loader->registerClasses(
 	[
 		'NotFound' => APP_PATH.'/middlewares/NotFound.php',
-		//'Phalcon\Http' => APP_PATH.'/Phalcon/Http/',
-		//'IndexController'         => APP_PATH.'/backend/controllers/IndexController.php',
-		//'AquariumController'         => APP_PATH.'/backend/controllers/AquariumController.php',
-		//'Example\Base' => 'vendor/example/adapters/Example/BaseClass.php',
 	]
 );
 
 $loader->register();
-//$loader->registerDirs(
-//    array(
-//        $config->application->modelsDir
-//    )
-//)->register();
